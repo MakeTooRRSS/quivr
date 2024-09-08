ef7aa80a706d6e2f9c162e3f2d7e177350e9a952
MakeTooRRSS <maketoo.rrss@gmail.com>
2024-09-08 12:28:31 +0200

    changes

--- frontend/lib/components/Stripe/PricingModal/components/PricingTable/PricingTable.tsx
+++ frontend/lib/components/Stripe/PricingModal/components/PricingTable/PricingTable.tsx
@@ -11,7 +11,7 @@ export const StripePricingTable = ({ user_email }: { user_email: string }): JSX.
       <div className={styles.info_content}>
         <MessageInfoBox type="info" unforceWhite={true}>
           <div>
-            {"The free tier allows you to have"}
+            {"Your plan allows you to have"}
             <span className={styles.bold}> 3 brains </span>
             {"and"}
             <span className={styles.bold}> 25 chat credits </span>
