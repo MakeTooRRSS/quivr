861ff1e4d97f77dff428bdc7d2756f4835cfbdca
MakeTooRRSS <maketoo.rrss@gmail.com>
2024-09-08 14:54:12 +0200

    changed

--- frontend/lib/components/Menu/components/AnimationDiv.tsx
+++ frontend/lib/components/Menu/components/AnimationDiv.tsx
@@ -15,13 +15,14 @@ export const AnimatedDiv = ({ children }: AnimatedDivProps): JSX.Element => {
         width: isOpened ? OPENED_MENU_WIDTH : "0px",
       }}
       animate={{
-        width: isOpened ? OPENED_MENU_WIDTH : 0,
+        width: OPENED_MENU_WIDTH,
         opacity: isOpened ? 1 : 0.5,
         boxShadow: isOpened
           ? "10px 10px 16px rgba(0, 0, 0, 0)"
           : "10px 10px 16px rgba(0, 0, 0, 0.5)",
       }}
       className={"overflow-hidden flex flex-col flex-1 bg-grey"}
+
     >
       {children}
     </motion.div>
